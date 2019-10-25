64e898b9c6359d43fe1468dcdb370a90f1c0d777
adamelsbury <adam.john.elsbury@gmail.com>
2019-10-25 15:40:42 +0100

    submission form finished

--- js/main.js
+++ js/main.js
@@ -10,6 +10,19 @@ function closeMenu() {
   document.getElementById("menu").style.top = "-2000px";
 }
 
+function showSubmission() {
+  document.getElementById("submission-container").style.opacity = "1";
+  document.getElementById("submission-container").style.zIndex = "2";
+  document.getElementById("submission-container").style.right = "0";
+}
+
+function closeSubmission() {
+  document.getElementById("submission-container").style.opacity = "0";
+  document.getElementById("submission-container").style.zIndex = "-1";
+  document.getElementById("submission-container").style.right = "-2000px";
+}
+
+
 const slider = tns({
   container: '#my-slider',
   items: 1,
