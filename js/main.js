59c2b6d0c8928e3b0da55ec5ab1405ae68e405be
adamelsbury <adam.john.elsbury@gmail.com>
2019-10-29 18:54:42 +0000

    panel section working with javascript on desktop

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+const slider = tns({
+  container: '#my-slider',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  speed: 800,
+  controlsContainer: "#customize-controls",
+  mouseDrag: true,
+  });
+
+  const slider2 = tns({
+    container: '#my-slider2',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    speed: 800,
+    controlsContainer: "#customize-controls2",
+    mouseDrag: true,
+    });
+
+
 function showMenu() {
   document.getElementById("menu").style.opacity = "1";
   document.getElementById("menu").style.zIndex = "2";
@@ -22,23 +43,29 @@ function closeSubmission() {
   document.getElementById("submission-container").style.right = "-2000px";
 }
 
+function showPanelOne() {
+  document.getElementById("panel-switch-1").style.opacity = "1";
+  document.getElementById("panel-switch-1").style.zIndex = "2";
+  document.getElementById("panel-switch-2").style.opacity = "0";
+  document.getElementById("panel-switch-2").style.zIndex = "-1";
+  document.getElementById("panel-switch-3").style.opacity = "0";
+  document.getElementById("panel-switch-3").style.zIndex = "-1";
+}
 
-const slider = tns({
-  container: '#my-slider',
-  items: 1,
-  slideBy: 'page',
-  autoplay: false,
-  speed: 800,
-  controlsContainer: "#customize-controls",
-  mouseDrag: true,
-  });
+function showPanelTwo() {
+  document.getElementById("panel-switch-1").style.opacity = "0";
+  document.getElementById("panel-switch-1").style.zIndex = "-1";
+  document.getElementById("panel-switch-2").style.opacity = "1";
+  document.getElementById("panel-switch-2").style.zIndex = "2";
+  document.getElementById("panel-switch-3").style.opacity = "0";
+  document.getElementById("panel-switch-3").style.zIndex = "-1";
+}
 
-  const slider2 = tns({
-    container: '#my-slider2',
-    items: 1,
-    slideBy: 'page',
-    autoplay: false,
-    speed: 800,
-    controlsContainer: "#customize-controls2",
-    mouseDrag: true,
-    });
+function showPanelThree() {
+  document.getElementById("panel-switch-1").style.opacity = "0";
+  document.getElementById("panel-switch-1").style.zIndex = "-1";
+  document.getElementById("panel-switch-2").style.opacity = "0";
+  document.getElementById("panel-switch-2").style.zIndex = "-1";
+  document.getElementById("panel-switch-3").style.opacity = "1";
+  document.getElementById("panel-switch-3").style.zIndex = "2";
+}
